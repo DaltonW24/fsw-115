16e016ab695856a658d99d3de60baa5e2cfd822e
DaltonW24 <dalton.welch@bryanuniversity.edu>
2022-08-17 10:24:26 -0400

    pokemon assignment done

--- Week3/Assignment.js
+++ Week3/Assignment.js
@@ -1,11 +1,21 @@
 function loadDoc() {
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-       document.getElementById("list").innerHTML = this.responseText;
-      }
-    };
-    xhttp.open("GET", "https://pokeapi.co/api/v2/", true);
+    xhttp.open("GET", "https://pokeapi.co/api/v2/pokemon/", true);
     xhttp.send();
+    xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      let data = JSON.parse(xhttp.responseText)
+      showData(data.results)
+    }
+    };
+    function showData(data){
+      for(i = 0; i < data.length; i++){
+      const pokemon = document.createElement("h1")
+      pokemon.innerHTML = data[i].name
+      document.body.appendChild(pokemon)
+      console.log(data)
+      }
   }
-  
+  } 
+  // Do i even have this written right? I looked on w3 schools, and i think i have this all right, but i can't get it to load either
+  loadDoc()
